2b6e0f864138160eb3059641f57e8f6d3e1e21bd
Joy Cheng <xuejiao.j.cheng@gmail.com>
2018-06-04 21:08:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,30 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var newItem = {};
+ newItem.itemName = item;
+ newItem.itemPrice = Math.floor(Math.random() * 99) + 1;
+ cart.push(newItem);
+ return `${item} has been added to your cart.`
+ 
 }
 
 function viewCart() {
   // write your code here
+  if (cart.length ===0){
+    return "Your shopping cart is empty."
+  }
+  
+  else{
+  var newArr = [];
+  for(var n=0; n<cart.length-1; n++){
+    newArr.push(`${cart[n][itemName]} at ${cart[n][itemPrice]}`)
+    
+  }
+  newArr = [...newArr.slice(0, newArr.length), 'and', ...newArr[newArr.length]]
+  var string = newArr.join(', ');
+  return `In your cart, you have ` + string;
+}
 }
 
 function total() {
